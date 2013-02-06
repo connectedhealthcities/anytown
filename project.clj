8be2277bb5737e932fa694ae0e8bd463a2a75015
James Cunningham <j.a.cunningham@gmail.com>
2013-02-06 15:03:21 +0000

    Add missing libraries to project.clj.
    Add data.priority-map and algo.generic.functor libraries for use in pathway.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/data.priority-map "0.0.2"]
+                 [org.clojure/algo.generic "0.1.0"]])
