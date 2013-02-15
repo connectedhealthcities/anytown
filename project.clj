4a23301f58dafa7c7ebb4a17febc784a01e6a626
James Cunningham <j.a.cunningham@gmail.com>
2013-02-15 14:55:15 +0000

    Project file update.

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject simulator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject simulator "1.0.0"
+  :description "Tool for simulating large corpuses of patient data."
+  :url "http://nweh.org.uk/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.priority-map "0.0.2"]
-                 [org.clojure/algo.generic "0.1.0"]])
+                 [org.clojure/algo.generic "0.1.0"]
+                 [incanter "1.4.0"]]
+  :jvm-opts ["-Djava.awt.headless=true"])
