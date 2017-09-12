efdebe4858ca2866a88e7515302094a7007cafa2
James Cunningham <j.a.cunningham@gmail.com>
2017-09-12 11:54:32 +0100

    Bump library versions

--- src/patient-simulator/project.clj
+++ src/patient-simulator/project.clj
@@ -3,10 +3,10 @@
   :url "https://www.connectedhealthcities.org/"
   :license {:name "University of Manchester"
             :url "http://umip.com/"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.priority-map "0.0.2"]
-                 [org.clojure/algo.generic "0.1.0"]
-                 [org.clojure/tools.namespace "0.2.3"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.priority-map "0.0.7"]
+                 [org.clojure/algo.generic "0.1.2"]
+                 [org.clojure/tools.namespace "0.2.11"]
                  [incanter "1.4.0"]]
   :jvm-opts ["-Djava.awt.headless=true"]
   :aot [simulator.core]
