a624de73ede4b2615adab82e10b68176d699ed01
James Cunningham <j.a.cunningham@gmail.com>
2017-09-12 10:17:19 +0100

    Update copyright information

--- src/patient-simulator/project.clj
+++ src/patient-simulator/project.clj
@@ -1,8 +1,8 @@
 (defproject simulator "1.0.0"
   :description "Tool for simulating large corpuses of patient data."
-  :url "http://nweh.org.uk/"
-  :license {:name "Copyright NWEH (2013)"
-            :url "http://nweh.org.uk"}
+  :url "https://www.connectedhealthcities.org/"
+  :license {:name "University of Manchester"
+            :url "http://umip.com/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.priority-map "0.0.2"]
                  [org.clojure/algo.generic "0.1.0"]
